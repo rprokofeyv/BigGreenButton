911ea20407c90949e2c1f8df8379d1d1af10cc23
Roman Prokofyev (Mariupol) <r.prokofeyv@mobidev.biz>
2018-05-01 15:16:50 +0300

    add converter

---
+++ BigGreenButton/BigGreenButton/Converters/WidthToRadiusConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BigGreenButton.Converters
+{
+    class WidthToRadiusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = 0;
+            if(int.TryParse(value.ToString(), out int intValue)){
+                result = intValue / 2;
+            }
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+            // Optional: throw new NotSupportedException();
+        }
+    }
+}
