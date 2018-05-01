4938a4b3b9a64de2e3fe80ba3c88ce7dca250112
Roman Prokofyev (Mariupol) <r.prokofeyv@mobidev.biz>
2018-05-01 14:07:29 +0300

    initial commit

---
+++ BigGreenButton/BigGreenButton.Android/Renders/EllipsButtonRender.cs
@@ -0,0 +1,16 @@
+﻿using Android.Content;
+using BigGreenButton.Droid.Renders;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Button), typeof(EllipsButtonRender))]
+namespace BigGreenButton.Droid.Renders
+{
+    public class EllipsButtonRender : ButtonRenderer
+    {
+        public EllipsButtonRender(Context context) : base(context)
+        {
+
+        }
+    }
+}
